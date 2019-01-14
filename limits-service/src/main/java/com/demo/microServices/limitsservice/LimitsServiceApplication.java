e6a197c4e10f8cace6be42adb7964648e6d9826e
chandan.thakur <chandan.thakur@dreamorbit.com>
2019-01-14 14:24:41 +0530

    changes

--- limits-service/src/main/java/com/demo/microServices/limitsservice/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/demo/microServices/limitsservice/LimitsServiceApplication.java
@@ -3,7 +3,7 @@ package com.demo.microServices.limitsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class LimitsServiceApplication {
 
 	public static void main(String[] args) {
